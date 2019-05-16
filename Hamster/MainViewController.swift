68f90b8deaaa4a778d2374a76cb1072b796c927b
Frank Cheng <ufo22940268@gmail.com>
2019-05-16 15:55:54 +0800

    v

--- Hamster/MainViewController.swift
+++ Hamster/MainViewController.swift
@@ -17,7 +17,7 @@ class MainViewController: UIViewController, UITableViewDataSource {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
 
-        records = [Record(host: "v2ex.com")]
+        records = [Record(host: "v2ex.com", url: "v2ex.com/detail")]
         tableView.register(RecordCell.self, forCellReuseIdentifier: "cell")
         tableView.tableFooterView = UIView(frame: CGRect(origin: .zero, size: CGSize(width: view.bounds.width, height: 1)))
     }
@@ -30,6 +30,9 @@ class MainViewController: UIViewController, UITableViewDataSource {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! RecordCell
         let record = records[indexPath.row]
         cell.textLabel?.text = record.host
+        cell.detailTextLabel?.text = record.url
+        let imageSize = CGRect(origin: .zero, size: CGSize(width: cell.bounds.height, height: cell.bounds.height)).insetBy(dx: 8, dy: 8).size
+        cell.imageView?.image = record.capitalImage(on: imageSize)
         return cell
     }
     
--- Hamster/RecordCell.swift
+++ Hamster/RecordCell.swift
@@ -10,19 +10,49 @@ import UIKit
 
 struct Record {
     var host: String
+    var url: String
+    
+    func capitalImage(on size: CGSize) -> UIImage {
+        return UIGraphicsImageRenderer.init(size: size).image { context in
+            let path = UIBezierPath(rect: CGRect(origin: .zero, size: size))
+            UIColor.lightGray.setFill()
+            path.fill()
+
+            let label = UILabel(frame: CGRect(origin: .zero, size: size))
+            label.text = capitalCharacter
+            label.textColor = .white
+            label.adjustsFontSizeToFitWidth = true
+            label.textAlignment = .center
+            label.layer.render(in: context.cgContext)
+        }
+    }
+    
+    var capitalCharacter: String {
+        if let first = host.first {
+            return String([first])
+        } else {
+            return ""
+        }
+    }
 }
 
 class RecordCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        imageView?.layer.cornerRadius = 4
+        imageView?.clipsToBounds = true        
     }
-
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
+        backgroundColor = selected ? UIColor.lightGray : .clear
     }
 
 }
